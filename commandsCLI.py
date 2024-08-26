870626f16fe0a75974c4f98c9f2a7b35ebf70c5c
luisca12 <solera.luis12@gmail.com>
2024-08-26 13:49:20 -0400

    Improved code

--- commandsCLI.py
+++ commandsCLI.py
@@ -1,9 +1,6 @@
 from netmiko import ConnectHandler
 from log import authLog
 
-import traceback
-import os
-
 def passAudit(validIPs, username, netDevice):
     # This function is to take a show run
 
@@ -25,21 +22,13 @@ def passAudit(validIPs, username, netDevice):
 
             print(f"Connecting to device {validDeviceIP}...")
             with ConnectHandler(**currentNetDevice) as sshAccess:
-                try:
-                    sshAccess.enable()
-
-                except Exception as error:
-                    print(f"An error occurred: {error}\n")
-                    authLog.error(f"User {username} with password {currentNetDevice['password']} connected to {validDeviceIP} got an error: Authentcation failed.")
-                    with open(f"failedDevices.txt","a") as failedDevices:
-                        failedDevices.write(f"User {username} with password {currentNetDevice['password']} connected to {validDeviceIP} got an error: Authentcation failed.\n")
-
+                authLog.info(f"Successfully connected to {validDeviceIP}")
+                    
         except Exception as error:
-            print(f"ERROR: An error occurred: {error}\n", traceback.format_exc())
-            authLog.error(f"User {username} connected to {validDeviceIP} got an error: {error}")
-            authLog.error(traceback.format_exc(),"\n")
+            print(f"An error occurred: {error}\n")
+            authLog.error(f"User {username} with password {currentNetDevice['password']} connected to {validDeviceIP} got an error: Authentcation failed.")
             with open(f"failedDevices.txt","a") as failedDevices:
-                failedDevices.write(f"User {username} connected to {validDeviceIP} got an error.\n")
+                failedDevices.write(f"User {username} with password {currentNetDevice['password']} connected to {validDeviceIP} got an error: Authentcation failed.\n")
 
         print(f"Outputs and files successfully created for device {validDeviceIP}.\n")
         print("For any erros or logs please check Logs -> authLog.txt\n")
